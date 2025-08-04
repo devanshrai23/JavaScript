b7587a1024da7d9be529235f9be28dd9f1c69fe3
Devansh Rai <devanshrai2385@gmail.com>
2025-08-04 19:32:31 +0000

    Operations discussed

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -28,3 +28,16 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(typeof stringNumber);
 console.log(stringNumber);
+
+// ********************** Operations *********************
+let str1 = "hello";
+let str2 = " Devansh";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log(1 + "2");      // 12
+// console.log("1" + 2);      // 12
+// console.log("1" + 2 + 2);  // 122 -> (string first)
+// console.log(2 + 2 + "1");  // 41
+
